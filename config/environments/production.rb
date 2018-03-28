a06e0b9887dc5c747beb6eea47371c25783dc9d1
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-03-28 20:09:30 +0530

    google 2 way auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,7 @@ Rails.application.configure do
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
    :authentication       => "plain",
-  :enable_starttls_auto => true
+   :enable_starttls_auto => true ,
+   tls: true
   }
 end
