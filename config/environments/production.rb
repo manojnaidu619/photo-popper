305d278232e4c67570e2644e69b9a8e2d1157da4
Manoj_Naidu619 <manojnaidu619@gmail.com>
2018-03-28 22:31:14 +0530

    another bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :domain               => 'heroku.com'
+   :domain               => 'heroku.com',
    :user_name            => ENV['gmail_username'],
    :password             => ENV['gmail_password'],
    :authentication       => "plain",
